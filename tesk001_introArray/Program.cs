c60758380887dcde2466a7a49d91d84dcf47af24
Roman <roma.klimov10.94@gmail.com>
2023-04-17 00:14:41 +1100

    сохранил половину лекции

---
+++ tesk001_introArray/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Напишем программу котороая будет находить максимальное число из 9 чисел
+Console.Clear();
+int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {51, 32, 353, 44, 95, 16, 47, 28, 9,};
+
+int answer = max(
+    max(array[0], array[1], array[2]),
+    max(array[3], array[4], array[5]),
+    max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(answer);
+
+
+
+
+
--- tesk001_max1_1/Program.cs
+++ tesk001_max1_1/Program.cs
@@ -1,2 +1,34 @@
 ﻿// Напишем программу котороая будет находить максимальное число из 9 чисел
 
+int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int number1 = 3;
+int number2 = 5;
+int number3 = 12;
+int number4 = 1555;
+int number5 = 10;
+int number6 = 2;
+int number7 = 25;
+int number8 = 33;
+int number9 = 9;
+
+/*int max1 = max(number1, number2, number3) ;
+int max2 = max(number4, number5, number6) ;
+int max3 = max(number7, number8, number9) ;
+int answer = max(max1, max2, max3);
+
+мы можем внутри аргумента передать
+
+*/
+
+int answer = max(max(number1, number2, number3),
+    max(number4, number5, number6),
+    max(number7, number8, number9));
+
+Console.WriteLine(answer);
