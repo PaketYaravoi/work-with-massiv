89fdadd36219534bd566bb7af5bf83748f192944
Roman <roma.klimov10.94@gmail.com>
2023-04-17 21:31:32 +1100

    рабочая версия приложения

--- tesk001_ArrayLibrary/Program.cs
+++ tesk001_ArrayLibrary/Program.cs
@@ -24,23 +24,27 @@ int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
     while(index < count)
     {
         if(collection[index] == find)
         {
-            position = index;    
+            position = index;
+            break;    
         }
-    
-    }   index++;
+        index++;
+    }
     return position;
 
 }
+
 
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
 
+int pos = IndexOf(array, 22);
 
-int pos = IndexOf(array, 1);
+Console.WriteLine(pos);
