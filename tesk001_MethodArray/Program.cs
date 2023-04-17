8f915c84cba51d7e55f140ca6b619ec0109829f4
Roman <roma.klimov10.94@gmail.com>
2023-04-17 14:00:15 +1100

    Переписал задачу с испльзованием метода tesk001_MethodArray

---
+++ tesk001_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Поиск позиции нужного нам елемента
+Console.Clear();
+int[] array = {51, 32, 73, 51, 15, 326, 51, 788, 29};
+
+int n = array.Length;
+int find = 51;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {   
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+    
+}
